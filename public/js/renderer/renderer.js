5cd749e9afdf6c1d05891bcb4b737fdf119aee2f
Konstantin Barkalov <mail@barkalov.ru>
2018-04-18 14:33:33 +0700

    doing split

--- public/js/renderer/renderer.js
+++ public/js/renderer/renderer.js
@@ -1,17 +1,17 @@
 'use strict';
 // ES6
 import Limiter from './limiter.js';
-import RingedRenderer from './ringedRenderer.js';
+import ParticedRenderer from './particedRenderer';
 import Helper from './helper.js';
 
 let safemod = Helper.safemod;
-class Renderer extends RingedRenderer {
+class Renderer extends ParticedRenderer {
   static get _defaultInitialOptions() {
     return Object.assign({}, super._defaultInitialOptions, {
       ionica: null,
-      particDynasMaxCount: 2048,
-      particFatsMaxCount: 8,
-      particHeroesMaxCount: 1,
+      particDynasMaxCount: 2048, // RingedRenderer based
+      particFatsMaxCount: 8, // RingedRenderer based
+      particHeroesMaxCount: 1, // RingedRenderer based
       fps: 60,  // AbstractIterativeRenderer based
     });
   }
@@ -45,12 +45,6 @@ class Renderer extends RingedRenderer {
     }
     super._construct();
     this._input = this._initialOptions.ionica._input;
- 
-    this._partic = {
-      dynas: new Float32Array(this._initialOptions.particDynasMaxCount * 8),
-      fats: new Float32Array(this._initialOptions.particFatsMaxCount * 6),
-      heroes: new Float32Array(this._initialOptions.particHeroesMaxCount * 6),
-    }
 
     this._limiter = new Limiter({coreconfigKey: this._runtimeOptions.coreconfigKey});
     
@@ -58,13 +52,8 @@ class Renderer extends RingedRenderer {
 
   _reset() {
     super._reset();
-    this._resetPartic();
   }
-  _resetPartic() {
-    this._fillParticDynasRandom();
-    this._fillParticFatsRandom();
-    this._fillParticHeroesRandom();
-  }
+
 
   _iteration() {
     super._iteration();
@@ -123,39 +112,6 @@ class Renderer extends RingedRenderer {
       this._ring.ph.flow[i] += (1 - dimFlowRatio) * pumpPower * this._initialOptions.masterPixelCount
     }
       
-  }
-
-  _fillParticDynasRandom() {
-    for (let i = 0; i < this._initialOptions.particDynasMaxCount; i++) {
-      this._partic.dynas[i * 8 + 0] = Math.random() * this._runtimeOptions.particDynasAverageTtl * 2;
-      this._partic.dynas[i * 8 + 1] = Math.random() * this._initialOptions.masterPixelCount;
-      this._partic.dynas[i * 8 + 2] = Math.random() - 0.5;
-      this._partic.dynas[i * 8 + 3] = Math.random();
-      this._partic.dynas[i * 8 + 4] = Math.random();
-      this._partic.dynas[i * 8 + 5] = Math.random();
-      this._partic.dynas[i * 8 + 6] = 0; // burnTtl
-      this._partic.dynas[i * 8 + 7] = Math.random(); // entropy
-    }
-  }
-  _fillParticFatsRandom() {
-    for (let i = 0; i < this._initialOptions.particFatsMaxCount; i++) {
-      this._partic.fats[i * 6 + 0] = 0; //TODO drop
-      this._partic.fats[i * 6 + 1] = Math.random() * this._initialOptions.masterPixelCount;
-      this._partic.fats[i * 6 + 2] = (Math.random() - 0.5) * 500;
-      this._partic.fats[i * 6 + 3] = Math.random();
-      this._partic.fats[i * 6 + 4] = Math.random();
-      this._partic.fats[i * 6 + 5] = Math.random();
-    }
-  }
-  _fillParticHeroesRandom() {
-    for (let i = 0; i < this._initialOptions.particHeroesMaxCount; i++) {
-      this._partic.heroes[i * 6 + 0] = 0; //TODO drop
-      this._partic.heroes[i * 6 + 1] = Math.random() * this._initialOptions.masterPixelCount;
-      this._partic.heroes[i * 6 + 2] = (Math.random() - 0.5) * 500;
-      this._partic.heroes[i * 6 + 3] = Math.random();
-      this._partic.heroes[i * 6 + 4] = Math.random();
-      this._partic.heroes[i * 6 + 5] = Math.random();
-    }
   }
 
   _liveParticFats() {
